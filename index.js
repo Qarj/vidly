887aa830002eeca8908ab0e5a5cd9181a733b1ca
Tim Buckland <tim.buckland@gmail.com>
2020-02-25 22:25:17 +0000

    added cors, bugfix movie post put, added email and name to jwt

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const winston = require('winston');
-const express = require('express');
+const winston = require("winston");
+const express = require("express");
 const app = express();
 
-require('./startup/logging')();
-require('./startup/routes')(app);
-require('./startup/db')();
-require('./startup/config')();
-require('./startup/validation')();
-require('./startup/prod')(app);
+require("./startup/logging")();
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+require("./startup/prod")(app);
 
 const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => winston.info(`Listening on port ${PORT}...`));
+const server = app.listen(PORT, () =>
+  winston.info(`Listening on port ${PORT}...`)
+);
 
 module.exports = server;
